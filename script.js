5b3ea2f5be51408837addc582c339b5efd775984
ranjhal23 <ranjhal2003@gmail.com>
2024-02-26 13:10:03 +0530

    js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const inputSlider= document.querySelector("[data-lengthSlider]");
 const lengthDisplay= document.querySelector("[length-Display]");
 const passwordDisplay= document.querySelector("[data-passwordDisplay]");
-const copyButton= document.querySelector("")
+const copyButton= document.querySelector("[data-copy]");
+const copyMsg= document.querySelector("[data-copyMsg]");
+const uppercaseCheck= document.querySelector("#uppercase");
+const lowercaseCheck= document.querySelector("#lowercase");
+const numbersCheck= document.querySelector("#numbers");
+const symbolsCheck= document.querySelector("#symbols");
+const indicator= document.querySelector(".indicator");
+const generateBtn= document.querySelector("#generateBtn");
+const allcheckbox= document.querySelectorAll("input[type= checkbox]");
+let password="";
+let passwordlength=10;
+let checkCount=1;
