c53b9d8f053a866bf46c719ab3ba77ab9bf63908
ranjhal23 <ranjhal2003@gmail.com>
2024-02-26 12:37:58 +0530

    js

---
+++ script.js
@@ -0,0 +1,4 @@
+const inputSlider= document.querySelector("[data-lengthSlider]");
+const lengthDisplay= document.querySelector("[length-Display]");
+const passwordDisplay= document.querySelector("[data-passwordDisplay]");
+const copyButton= document.querySelector("")
